85df90028f94aa013a20b75a505a1e16f3baf2ba
CarlosBerry <99772893+CarlosBerry@users.noreply.github.com>
2023-01-07 17:33:31 -0500

    LogPress in PB

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton({children}) {
+    function PressHandler(){
+        console.log('pressed!!');
+    }
     return(
         <View>
+            <Pressable onPress={PressHandler}>
             <Text>{children}</Text>
+            </Pressable>
+            
         </View>
     );
 
 }
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    
+});
+    
