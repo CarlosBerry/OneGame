e980a36865f3b5618aa61f2612b66eb442510164
CarlosBerry <99772893+CarlosBerry@users.noreply.github.com>
2023-01-07 15:23:32 -0500

    buttons without styling

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,3 +1,15 @@
-function StartGameScreen(){}
+import { TextInput, View} from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
+
+function StartGameScreen(){
+    return (
+        <View>
+            <TextInput/>
+            <PrimaryButton>Reset</PrimaryButton>
+            <PrimaryButton>Confirm</PrimaryButton>
+
+        </View>
+    );
+}
 
 export default StartGameScreen;
