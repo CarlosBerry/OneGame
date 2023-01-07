58f0cb48907ca8f2f2f91bf5fa79ee8d488b5fca
CarlosBerry <99772893+CarlosBerry@users.noreply.github.com>
2023-01-07 17:56:15 -0500

    ripple_Effect

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -5,9 +5,12 @@ function PrimaryButton({children}) {
         console.log('pressed!!');
     }
     return(
-        <View>
-            <Pressable onPress={PressHandler}>
-            <Text>{children}</Text>
+        <View style={styles.outterContainer}>
+            <Pressable 
+            style={styles.innerContainer}
+            onPress={PressHandler} 
+            android_ripple={{color: '#640233'}}>
+                 <Text style={styles.buttonText}>{children}</Text>
             </Pressable>
             
         </View>
@@ -17,6 +20,21 @@ function PrimaryButton({children}) {
 export default PrimaryButton;
 
 const styles = StyleSheet.create({
-    
+    innerContainer: {
+        backgroundColor: '#4e0329',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2
+    },
+    outterContainer:{
+        borderRadius: 32,
+        margin: 4,
+        overflow: 'hidden'
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    }
+
 });
     
