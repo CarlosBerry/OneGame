52fd399bc53e670ccd36a7898fcb82470a331855
CarlosBerry <99772893+CarlosBerry@users.noreply.github.com>
2023-01-09 15:11:48 -0500

    linearGradient

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StyleSheet, View} from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import StartGameScreen from './screens/StartGameScreen';  
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return  (
-    <View style={styles.screenRoots}>
+    <LinearGradient colors={['#Fe0329','#ddb52f']}
+    style={styles.screenRoots}>
       <StartGameScreen/>
-    </View>
+    </LinearGradient>
   
   );
 }
