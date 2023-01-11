a2c0c938c7b5b6bc81d9f22602cc810f3abb7536
CarlosBerry <99772893+CarlosBerry@users.noreply.github.com>
2023-01-11 09:28:26 -0500

    adding game control buttons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { Text,View, StyleSheet } from "react-native";
+import { Text,View, StyleSheet, Alert } from "react-native";
 import NumberContainer from "../components/Game/NumbersContainer";
 import Title from "../components/Title";
+import PrimaryButton from "../components/PrimaryButton";
 
 function genRndBetween(min, max, exclude){
     const rndNum = Math.floor(Math.random()*(max-min))+min;
@@ -12,10 +13,33 @@ function genRndBetween(min, max, exclude){
         return rndNum;
     }
 }
+let minBoundary = 1;
+let maxBoundary = 100;
+
 
 function GameScreen({userNumber}){
-    const initialGuess= genRndBetween(1,100, userNumber);
+    const initialGuess= genRndBetween(minBoundary,maxBoundary, userNumber);
     const[currentGuess, setCurrentGuess]=useState(initialGuess);
+    
+
+    function nextGuestHandler(direction){
+
+        if ((direction === 'lower' && currentGuess < userNumber) ||
+        (direction === 'greater' && currentGuess > userNumber))
+        {
+            Alert.alert('Do not lie!', 'you know that is not possible',[{Text:'sorry', style:'cancel'},
+        ]);
+        return;
+        }
+    
+        if(direction==='lower'){
+           maxBoundary = currentGuess;
+        } else{
+            minBoundary = currentGuess+1;
+        }
+        const newReadNumber = genRndBetween(minBoundary, maxBoundary, currentGuess);
+        setCurrentGuess(newReadNumber);
+    }
 
   return  (
   <View style={styles.screen}>
@@ -25,11 +49,18 @@ function GameScreen({userNumber}){
     </View>
     <View>
         <Text>Higher or lower?</Text>
+    
+    <View>
+        <PrimaryButton onPress={nextGuestHandler.bind(this,'lower')}>-</PrimaryButton>
+        <PrimaryButton onPress={nextGuestHandler.bind(this,'greater')}>++</PrimaryButton>
+    </View>
     </View>
     
   </View>
   );
 }
+
+
 
 export default GameScreen;
 
